dc14d4bb0a08f5087a80ec8c49fd8cab87910795
siyangzhang <siyang.zhang@uleth.ca>
2017-02-17 13:57:04 -0500

    Feb 17 commit

---
+++ app/src/main/java/com/example/siyangzhang/tradefree/MainActivity.java
@@ -0,0 +1,206 @@
+package com.example.siyangzhang.tradefree;
+
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.siyangzhang.tradefree.R;
+import com.example.siyangzhang.tradefree.fragment.IndexFragment;
+import com.example.siyangzhang.tradefree.fragment.CategFragment;
+import com.example.siyangzhang.tradefree.fragment.NearbyFragment;
+import com.example.siyangzhang.tradefree.fragment.ProfileFragment;
+
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private FragmentManager fm;
+
+    private LinearLayout mTabIndex, mTabClass, mTabFind, mTabMe;
+
+    private ImageView mIndexImg, mClassImg, mFindImg, mMeImg;
+
+    private Fragment tabindex, tabclass, tabfind, tabme;
+
+    private TextView title_text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        initEvent();
+        setSelect(0);
+    }
+
+    private void initView() {
+
+        mTabIndex = (LinearLayout) findViewById(R.id.id_index);
+        mTabClass = (LinearLayout) findViewById(R.id.id_class);
+        mTabFind = (LinearLayout) findViewById(R.id.id_find);
+        mTabMe = (LinearLayout) findViewById(R.id.id_me);
+
+        mIndexImg = (ImageView) findViewById(R.id.id_indeximg);
+        mClassImg = (ImageView) findViewById(R.id.id_classimg);
+        mFindImg = (ImageView) findViewById(R.id.id_findimg);
+        mMeImg = (ImageView) findViewById(R.id.id_meimg);
+
+        title_text = (TextView) findViewById(R.id.title_text);
+    }
+
+    private void initEvent() {
+        mTabIndex.setOnClickListener(this);
+        mTabClass.setOnClickListener(this);
+        mTabFind.setOnClickListener(this);
+        mTabMe.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.id_index:
+                setSelect(0);
+                break;
+            case R.id.id_class:
+                setSelect(1);
+                break;
+            case R.id.id_find:
+                setSelect(2);
+                break;
+            case R.id.id_me:
+                setSelect(3);
+                break;
+        }
+    }
+
+    private void setSelect(int i) {
+        switch (i) {
+            case 0:
+                if (tabindex == null) {
+                    tabindex = new IndexFragment();
+                }
+                changeFragment(tabindex);
+                break;
+            case 1:
+                if (tabclass == null) {
+                    tabclass = new CategFragment();
+                }
+                changeFragment(tabclass);
+                break;
+            case 2:
+                if (tabfind == null) {
+                    tabfind = new NearbyFragment();
+                }
+                changeFragment(tabfind);
+                break;
+            case 3:
+                if (tabme == null) {
+                    tabme = new ProfileFragment();
+                }
+                changeFragment(tabme);
+                break;
+        }
+        setTab(i);
+
+    }
+
+    private void setTab(int i) {
+        // 默认图片
+        resetImgs();
+        // 设置图片为亮色,切换内容区域
+        switch (i) {
+            case 0:
+                title_text.setVisibility(View.VISIBLE);
+                title_text.setClickable(true);
+                title_text.setText("The Ohio State University");
+                title_text.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "Currently unable to switch campus",
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
+                mIndexImg.setImageResource(R.drawable.index_pressed);
+                break;
+            case 1:
+                title_text.setVisibility(View.VISIBLE);
+                title_text.setClickable(false);
+                title_text.setText("Category");
+                mClassImg.setImageResource(R.drawable.classes_pressed);
+                break;
+            case 2:
+                title_text.setVisibility(View.VISIBLE);
+                title_text.setClickable(false);
+                title_text.setText("Nearby");
+                mFindImg.setImageResource(R.drawable.find_pressed);
+                break;
+            case 3:
+                title_text.setVisibility(View.VISIBLE);
+                title_text.setClickable(false);
+                title_text.setText("Profile");
+                mMeImg.setImageResource(R.drawable.me_pressed);
+                break;
+        }
+    }
+    private void changeFragment(Fragment targetFragment){
+        fm = getSupportFragmentManager();
+        if(fm!=null) {
+            fm.beginTransaction()
+                    .replace(R.id.fmcontent, targetFragment, "fragment")
+                    .setTransitionStyle(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
+                    .commit();
+        }
+    }
+    private void resetImgs() {
+        mIndexImg.setImageResource(R.drawable.index);
+        mClassImg.setImageResource(R.drawable.classes);
+        mFindImg.setImageResource(R.drawable.find);
+        mMeImg.setImageResource(R.drawable.me);
+    }
+
+    Boolean ActionSheetFlag = false;
+    private static long firstTime;
+
+    /**
+     * 连续按两次返回键就退出
+     */
+    @Override
+    public void onBackPressed() {
+        // TODO Auto-generated method stub
+        if (ActionSheetFlag) {
+            super.onBackPressed();
+        } else {
+            if (firstTime + 2000 > System.currentTimeMillis()) {
+                super.onBackPressed();
+            } else {
+                //ShowToast("再按一次退出程序");
+            }
+            firstTime = System.currentTimeMillis();
+        }
+    }
+
+    public void onIndexFragmentInteraction(Uri uri) {
+        // Do stuff
+    }
+    public void onCategFragmentInteraction(Uri uri) {
+        // Do stuff
+    }
+    public void onNearbyFragmentInteraction(Uri uri) {
+        // Do stuff
+    }
+    public void onProfileFragmentInteraction(Uri uri) {
+        // Do stuff
+    }
+}
---
+++ app/src/main/java/com/example/siyangzhang/tradefree/fragment/ProfileFragment.java
@@ -0,0 +1,127 @@
+package com.example.siyangzhang.tradefree.fragment;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.siyangzhang.tradefree.ItemActivity;
+import com.example.siyangzhang.tradefree.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ProfileFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ProfileFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ProfileFragment extends BaseFragment {
+
+    private View mView;
+
+    Button mBtn;
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ProfileFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ProfileFragment newInstance(String param1, String param2) {
+        ProfileFragment fragment = new ProfileFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    private View.OnClickListener mBtnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getActivity(), ItemActivity.class);
+            startAnimActivity(intent);
+        }
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //mBtn = (Button) mView.findViewById(R.id.commit_edit);
+        //mBtn.setOnClickListener(mBtnClickListener);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        mView =  inflater.inflate(R.layout.fragment_profile, container, false);
+        return mView;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onProfileFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            //throw new RuntimeException(context.toString()
+              //      + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onProfileFragmentInteraction(Uri uri);
+    }
+}
